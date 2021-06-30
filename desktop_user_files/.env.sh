dcf1231b199377b89dccef4974c5562b02b172b6
jld3103 <jld3103yt@gmail.com>
2021-06-30 18:27:31 +0200

    Update

--- desktop_user_files/.env.sh
+++ desktop_user_files/.env.sh
@@ -2,7 +2,8 @@ export PATH=$PATH:$HOME/flutter/bin
 export ANDROID_HOME=$HOME/Android/Sdk
 export ANDROID_NDK_HOME=$ANDROID_HOME/ndk/21.3.6528147/
 export ANDROID_NDK=$ANDROID_NDK_HOME
-export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/30.0.3
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/emulator
+#export PATH=$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/30.0.3
 export PATH=$PATH:$HOME/.pub-cache/bin
 export PATH=$PATH:$HOME/.cargo/bin
 export PATH=$PATH:$HOME/fuchsia/.jiri_root/bin
@@ -12,6 +13,7 @@ export PATH=$PATH:$HOME/src/depot_tools
 export PATH=$PATH:/opt/appimagetool/usr/bin
 export PATH=$PATH:$HOME/src/osxcross/target/bin
 export PATH=$PATH:$HOME/src/appimagetool/usr/bin
+export PATH=$PATH:/opt/dart-sdk-dev/bin/snapshots
 
 #export LC_NUMERIC="en_US.UTF-8"
 export PUB_CACHE=$HOME/.pub-cache
@@ -27,7 +29,7 @@ export LC_ALL=C
 
 export MAKEFLAGS="-j$(nproc)"
 
-export LOCAL_MIRROR=http://192.168.178.22:8080/\$repo/\$arch
+export LOCAL_MIRROR=http://192.168.178.21:8080/\$repo/\$arch
 
 # XanMod AUR settings
 export _microarchitecture=14
@@ -39,3 +41,5 @@ export USE_CCACHE=1
 export CCACHE_COMPRESS=1
 
 export CHROME_EXECUTABLE=chromium
+
+export DOCKER_PULL_POLICY=if-not-present
